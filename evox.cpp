cf12790ce43fe5a2d0eaa746819c18f34071b6c5
Quin <quin.marilyn05@gmail.com>
2024-02-07 13:38:31 -0700

    Complex expressions outside parthenesies work now. PEMDAS is still todo.

--- evox.cpp
+++ evox.cpp
@@ -1,69 +1,85 @@
 #include "evox.h"
 
-// Evaluates basic math expressions. Currently nothing advanced is supported, it's basically a four-function calculator. It'll need some refactoring to grow, but it's still quite doable, and planned.
 double evox(const std::string& expression) {
 	std::stack<double> numbers;
 	std::stack<char> operators;
+	bool reading_number = false;
+	double current_number = 0;
 	for (char c : expression) {
 		if (isdigit(c)) {
-			// Stupid and weird hack to convert a char to an int without a cast.
-			numbers.push(c - '0');
-		} else if (c == '+' || c == '-' || c == '*' || c == '/' || c == '^') {
-			while (!operators.empty() && operators.top() != '(' && (c == '^' || c == '*' || c == '/' || c == '+' || c == '-')) {
-				char op = operators.top();
-				double operand2 = numbers.top();
-				numbers.pop();
-				double operand1 = numbers.top();
-				numbers.pop();
-				switch (op) {
-				case '+':
-					numbers.push(operand1 + operand2);
-					break;
-				case '-':
-					numbers.push(operand1 - operand2);
-					break;
-				case '*':
-					numbers.push(operand1 * operand2);
-					break;
-				case '/':
-					numbers.push(operand1 / operand2);
-					break;
-				case '^':
-					numbers.push(pow(operand1, operand2));
-					break;
-				}
+			if (reading_number) {
+				current_number = current_number * 10 + (c - '0');
+			} else {
+				current_number = c - '0';
+				reading_number = true;
 			}
-			operators.push(c);
-		} else if (c == '(') {
-			operators.push(c);
-		} else if (c == ')') {
-			while (!operators.empty() && operators.top() != '(') {
-				char op = operators.top();
+		} else {
+			if (reading_number) {
+				numbers.push(current_number);
+				current_number = 0;
+				reading_number = false;
+			}
+			if (c == '+' || c == '-' || c == '*' || c == '/' || c == '^') {
+				while (!operators.empty() && operators.top() != '(' && (c == '^' || c == '*' || c == '/' || c == '+' || c == '-')) {
+					char op = operators.top();
+					double operand2 = numbers.top();
+					numbers.pop();
+					double operand1 = numbers.top();
+					numbers.pop();
+					switch (op) {
+					case '+':
+						numbers.push(operand1 + operand2);
+						break;
+					case '-':
+						numbers.push(operand1 - operand2);
+						break;
+					case '*':
+						numbers.push(operand1 * operand2);
+						break;
+					case '/':
+						numbers.push(operand1 / operand2);
+						break;
+					case '^':
+						numbers.push(pow(operand1, operand2));
+						break;
+					}
+					operators.pop();
+				}
+				operators.push(c);
+			} else if (c == '(') {
+				operators.push(c);
+			} else if (c == ')') {
+				while (!operators.empty() && operators.top() != '(') {
+					char op = operators.top();
+					operators.pop();
+					double operand2 = numbers.top();
+					numbers.pop();
+					double operand1 = numbers.top();
+					numbers.pop();
+					switch (op) {
+					case '+':
+						numbers.push(operand1 + operand2);
+						break;
+					case '-':
+						numbers.push(operand1 - operand2);
+						break;
+					case '*':
+						numbers.push(operand1 * operand2);
+						break;
+					case '/':
+						numbers.push(operand1 / operand2);
+						break;
+					case '^':
+						numbers.push(pow(operand1, operand2));
+						break;
+					}
+				}
 				operators.pop();
-				double operand2 = numbers.top();
-				numbers.pop();
-				double operand1 = numbers.top();
-				numbers.pop();
-				switch (op) {
-				case '+':
-					numbers.push(operand1 + operand2);
-					break;
-				case '-':
-					numbers.push(operand1 - operand2);
-					break;
-				case '*':
-					numbers.push(operand1 * operand2);
-					break;
-				case '/':
-					numbers.push(operand1 / operand2);
-					break;
-				case '^':
-					numbers.push(pow(operand1, operand2));
-					break;
-				}
 			}
-			operators.pop();
 		}
+	}
+	if (reading_number) {
+		numbers.push(current_number);
 	}
 	while (!operators.empty()) {
 		char op = operators.top();
