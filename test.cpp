a860f73a46fefb0c5259c5cf834a3f96cedea156
Quin <quin.marilyn05@gmail.com>
2024-02-07 13:12:50 -0700

    Make the tester allow you to input an expression on stdin.

--- test.cpp
+++ test.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
+#include <string>
 #include "evox.h"
 
 int main(int argc, const char **argv) {
-	std::cout << "Result is " << evox("((2+1)*4)/3") << std::endl;
+	std::string expression;
+	std::cout << "Enter expression:";
+	getline(std::cin, expression);
+	std::cout << evox(expression) << std::endl;
 	return 0;
 }
